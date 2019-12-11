3630118b4a7b4a4a63763c2e36761cc7a3540c7e
Raúl Rubén Castro Rivero <rrubencr@estudiantes.uci.cu>
2019-12-10 21:59:53 -0500

    CREATE Lideres de Picheo

--- bot.py
+++ bot.py
@@ -14,14 +14,15 @@ def getSoup():
     return soup
 
 
-def getResults():
+def getResultsToday():
     soup = getSoup()
-    runs = soup.find_all('span', {"class": "Mini_DayScore_Runs"})
+    data = soup.find_all('div', {"id": "MainContent_ctl01_UC_MDS_UpdatePanel"})
+    runs = data[0].find_all('span', {"class": "Mini_DayScore_Runs"})
     siglas = soup.find_all('span', {"class": "Mini_DayScore_Siglas"})
 
     results = ''
 
-    for i in range(0 , len(runs) // 2, 2):
+    for i in range(0 , len(runs), 2):
         results += ('%s %s - %s %s\n' % (siglas[i].text, runs[i].text, siglas[i + 1].text, runs[i + 1].text))
 
     return results
@@ -40,6 +41,20 @@ def getTabla():
     return tabla
 
 
+def getLideresIndividuales():
+    soup = getSoup()
+    nombres = soup.find_all('a', {"style": "margin-left: -15px; padding: 0; font-size: 15px; color: black;"})
+    departamento = soup.find_all('span', {"class": "lideres_widget_AAA_span"})
+    estadistica = soup.find_all('span', {"class": "lideres_widget_000_span"})
+
+    lideres = ''
+
+    for i in range(0, len(nombres)):
+        lideres += ('%s - %s %s \n' % (nombres[i].text, departamento[i].text, estadistica[i].text))
+
+    return lideres
+
+
 def tabla(bot, update):
     chat_id = update.message.chat_id
 
@@ -50,17 +65,25 @@ def tabla(bot, update):
 def resultados(bot, update):
     chat_id = update.message.chat_id
     
-    results = getResults()
+    results = getResultsToday()
 
     bot.send_message(chat_id=chat_id, text=results)
     bot.send_message(chat_id=chat_id, text='Si no aparecen los resultados completos pude que no se hallan celebrado los partidos aún.')
 
+
+def lideres(bot, update):
+    chat_id = update.message.chat_id
+
+    lideres = getLideresIndividuales()
+
+    bot.send_message(chat_id=chat_id, text=lideres)
 
 def main():
     updater = Updater(API_KEY)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('tabla', tabla))
     dp.add_handler(CommandHandler('resultados', resultados))
+    dp.add_handler(CommandHandler('lideres', lideres))
     updater.start_polling()
     updater.idle()
 
